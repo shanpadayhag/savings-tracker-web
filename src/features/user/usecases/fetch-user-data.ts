1c36da820ca3e3dede075e0b022aa8511635ed28
Shan Padayhag <shanpadayhag@gmail.com>
2026-02-07 22:16:32 +0100

    Added fetching of wallets

--- src/features/user/usecases/fetch-user-data.ts
+++ src/features/user/usecases/fetch-user-data.ts
@@ -1,19 +1,21 @@
 import JsonObj from '@/configs/types/json';
 import fetchCurrentUser from '@/features/user/api/fetch-current-user';
+import getWallets from '@/features/wallets/api/get-wallets';
 
 const fetchUserData = async () => {
   const userData: JsonObj = {};
 
   const user = await fetchCurrentUser();
   userData.user.id = user.id;
+  const wallets = await getWallets();
+  userData.wallets = wallets;
 
-  // fetch wallets
   // fetch goals
   // fetch goal_versions
   // fetch transactions
   // fetch transaction_entries
 
-  return userData
+  return userData;
 };
 
 export default fetchUserData;
--- src/features/wallets/repositories/wallet-repository.ts
+++ src/features/wallets/repositories/wallet-repository.ts
@@ -1,13 +1,13 @@
-import getWallets from '@/features/wallets/api/get-wallets';
+import getCachedWallets from '@/features/wallets/api/get-cached-wallets';
 import walletListItemModel from '@/features/wallets/models/wallet-list-item-model';
 import walletOptionModel from '@/features/wallets/models/wallet-option-model';
 
 const walletRepository = {
   async getWallets() {
-    return (await getWallets()).map(walletListItemModel.fromJson);
+    return (await getCachedWallets()).map(walletListItemModel.fromJson);
   },
   async getWalletOptions() {
-    return (await getWallets()).map(walletOptionModel.fromJson);
+    return (await getCachedWallets()).map(walletOptionModel.fromJson);
   },
 };
 
